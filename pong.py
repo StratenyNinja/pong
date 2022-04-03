c8e67bbaa6137ffdec0574adcf069a1857cb9a25
StratenyNinja <erik.stvrtecky2000@gmail.com>
2022-04-03 19:07:28 +0200

    Update pong.py

--- pong.py
+++ pong.py
@@ -2,3 +2,21 @@ import pygame
 import sys
 
 
+# Colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+# Main variables
+WINDOW_W = 730
+WINDOW_H = 530
+FPS = 60
+
+# Game variables
+BORDER_W = WINDOW_W
+BORDER_H = 10
+PADDLE_W = 10
+PADDLE_H = 100
+BALL_RADIUS = 10
+PADDLE_SPEED = 5
+BALL_SPEED = 5
+
