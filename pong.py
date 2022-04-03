ad0599abfa1dfff237bc30cd89c1a2b6f69dfbdf
StratenyNinja <erik.stvrtecky2000@gmail.com>
2022-04-03 19:09:28 +0200

    Update pong.py

--- pong.py
+++ pong.py
@@ -20,6 +20,24 @@ BALL_RADIUS = 10
 PADDLE_SPEED = 5
 BALL_SPEED = 5
 
+# Paddle
+class Paddle(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.Surface([PADDLE_W, PADDLE_H])
+        self.image.fill(WHITE)
+        self.rect = self.image.get_rect()
+        
+        pygame.draw.rect(self.image, WHITE, [0, 0, PADDLE_W, PADDLE_H])
+
+    def moveUp(self):
+        if self.rect.top > BORDER_H:
+            self.rect.top -= PADDLE_SPEED
+
+    def moveDown(self):
+        if self.rect.bottom < WINDOW_H - BORDER_H:
+            self.rect.bottom += PADDLE_SPEED
+
 pygame.init()
 window = pygame.display.set_mode((WINDOW_W, WINDOW_H))
 pygame.display.set_caption("Pong")
@@ -32,6 +50,15 @@ def draw_lines():
         if i % 2 == 0:
             pygame.draw.rect(window, WHITE, ((WINDOW_W - BORDER_H) // 2, i * BORDER_H, BORDER_H, BORDER_H))
 
+paddle1 = Paddle()
+paddle1.rect.x = PADDLE_W
+paddle1.rect.y = WINDOW_H // 2 - PADDLE_H // 2
+paddle2 = Paddle()
+paddle2.rect.x = WINDOW_W - PADDLE_W * 2
+paddle2.rect.y = WINDOW_H // 2 - PADDLE_H // 2
+all_sprites = pygame.sprite.Group()
+all_sprites.add(paddle1)
+all_sprites.add(paddle2)
 # Game loop
 while True:
     for event in pygame.event.get():
@@ -39,7 +66,19 @@ while True:
             pygame.quit()
             sys.exit()
     
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        paddle1.moveUp()
+    if keys[pygame.K_s]:
+        paddle1.moveDown()
+    if keys[pygame.K_UP]:
+        paddle2.moveUp()
+    if keys[pygame.K_DOWN]:
+        paddle2.moveDown() 
+
+    all_sprites.update()
     window.fill(BLACK)
     draw_lines()
+    all_sprites.draw(window)
     pygame.display.update()
     clock.tick(FPS)
