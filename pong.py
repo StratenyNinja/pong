2d9a7bd500d44fee5c288d601055f8b7a7eb8a2b
StratenyNinja <erik.stvrtecky2000@gmail.com>
2022-05-27 15:00:26 +0200

    Update pong.py

--- pong.py
+++ pong.py
@@ -227,12 +227,10 @@ class Manager:
             self.ball.speed[0] = -self.ball.speed[0]
     
     def restart_game(self):
-        self.ball.rect.center = (WINDOW_W // 2, WINDOW_H // 2)
         self.player1.rect.x = self.player1.rect.width * 2
-        self.player1.rect.y = WINDOW_H // 2
         self.player2.rect.x = WINDOW_W - self.player1.rect.width * 3
-        self.player2.rect.y = WINDOW_H // 2
-        self.ball.rect.x = (WINDOW_W - self.player1.rect.width) // 2
+        self.player1.rect.y = self.player2.rect.y = WINDOW_H // 2
+        self.ball.rect.x = (WINDOW_W - self.player1.rect.width * 2) // 2
         self.ball.rect.y = (WINDOW_H + STATS_H - self.ball.rect.height) // 2
         self.ball.speed = [random.choice([-BALL_SPEED, BALL_SPEED]), random.choice([-BALL_SPEED, BALL_SPEED])]
 
